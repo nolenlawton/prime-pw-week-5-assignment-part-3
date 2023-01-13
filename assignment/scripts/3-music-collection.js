ff0e7cc6ce7dc44e1fcd0b9d5109af6b51ecbf93
Nolen Lawton <mnlawton@icloud.com>
2023-01-12 18:50:38 -0600

    created a collection and added 6 objects to the array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,24 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection (title, artist, yearPublished) {
+    let record = {
+        title,
+        artist,
+        yearPublished
+    }
+    collection.push(record)
+}
+
+console.log(collection)
+
+addToCollection ('Good Kid, m.A.Ad city', 'Kendrick Lamar', 2012)
+addToCollection ('To Pimp a Butterfly.', 'Kendrick Lamar', 2015)
+addToCollection ('Beautiful Thugger Girls', 'Young Thug', 2017)
+addToCollection ('KOD', 'J. Cole', 2018)
+addToCollection ('Blonde', 'Frank Ocean', 2016)
+addToCollection ('Cozy Tapes Vol. 1', 'A$AP Mob', 2016)
+
+console.log(collection)
+ 
