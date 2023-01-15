617f2bbd9725b6e1ccca61501813c8856c42d882
Nolen Lawton <mnlawton@icloud.com>
2023-01-14 22:23:43 -0600

    very hard wow. could only get one object to push through.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -31,24 +31,6 @@ function findByArtist (artist = '') {
     return arrayOfArtist
 }
 
-// function search (propertyToSearchFor) {
-//     let arrayOfSearch = []
-//     console.log('Search for ' + propertyToSearchFor + ':')
-
-//     for (let record of collection) {
-//         if (record.title === propertyToSearchFor ||record.artist === propertyToSearchFor || record.yearPublished === propertyToSearchFor) {
-//             arrayOfSearch.push(record)  
-//         }
-//         else if(propertyToSearchFor === undefined || propertyToSearchFor === '') {
-//             console.log(collection)
-//             return collection
-//         }
-//     }
-
-//     console.log(arrayOfSearch)
-//     return arrayOfSearch
-// }
-
 let radioSong1 = {
     artist: 'Kendrick Lamar'
 }
@@ -60,60 +42,47 @@ let radioSong2 = {
 }
 
 let radioSong3 = {
-    artist: 'J. Cole',
-    yearPublished: 2014
+    artist: 'Elton John',
+    yearPublished: 2016
 }
-
-
 
 function search  (objectSearch) {
+    let propertiesFound = 0
+    let i = 0
+    let arrayOfSearch = []
     let properties = Object.keys(objectSearch) // object to array of properties
         // console.log(properties) // --- properties that are being searched for
-    
-    let propertiesFound = 0
 
-    let i = 0
+            for (let property of properties) {
+                console.log('searching for ' + property + ' ' + objectSearch[property]) // --- loops through properties of objectSearch
+                
+                    if (propertiesFound === 0) { 
 
-    let arrayOfSearch = []
+                        for (let object in collection) { 
+                            console.log(collection[object][property]) // --- loops through properties of collection
 
-        for (let property of properties) {
-            // console.log(property + ' is ' + objectSearch[property]) // --- loops through properties of objectSearch
-
-                if (propertiesFound === 0) { 
-            
-                    for (let object in collection) { 
-                        // console.log(property + ' is ' + collection[object][property]) // --- loops through properties of collection
-
-                    if (objectSearch[property] === collection[object][property]) {
-                            propertiesFound++ 
-                            break // if first objectSearch property is found in collection loop stops
-                        }
-                        else {
-                            i++ // if property is not found, go to the next index of collection
+                            if (objectSearch[property] === collection[object][property]) {
+                                propertiesFound++ 
+                                break // if first objectSearch property is found in collection loop stops
+                            }
+                            else {
+                                i++ // if property is not found, go to the next index of collection
+                            }
                         }
                     }
+                    else if (objectSearch[property] === collection[i][property]) { // using i to deal with specific index previously found
+                        propertiesFound++
+                    }
+            }
 
-                }
-
-                else if (objectSearch[property] === collection[i][property]) { // using i to deal with specific index previously found
-                    propertiesFound++
-                }
-        }
-
-    // console.log(propertiesFound) // --- how many properties were found
-    // console.log(i) // index of collection being dealt with
-
-    if (propertiesFound === properties.length) {
-        console.log('Match Found!')
-        arrayOfSearch.push(collection[i])
-        console.log(arrayOfSearch)
-    }
-    else {
-        console.log('No Match Found.')
-    }
+            if (propertiesFound == properties.length) {
+                console.log('Match Found!')
+                arrayOfSearch.push(collection[i])
+            }
+            console.log(arrayOfSearch)
 }
 
-addToCollection ('good kid, m.A.A.d city', 'Kendrick Lamar', 2012, 'Song 1', 120 , 'Song 2', 240)
+addToCollection ('good kid, m.A.A.d city', 'Kendrick Lamar', 2012)
 addToCollection ('To Pimp a Butterfly.', 'Kendrick Lamar', 2015)
 addToCollection ('Beautiful Thugger Girls', 'Young Thug', 2017)
 addToCollection ('KOD', 'J. Cole', 2018)
@@ -128,3 +97,6 @@ findByArtist('Elton John') // not in collection
 search(radioSong1)
 search(radioSong2)
 search(radioSong3)
+
+
+
